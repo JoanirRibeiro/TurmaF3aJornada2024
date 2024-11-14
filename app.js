99eefb160b3eddb902a2bce1d920a3b5fd0b7fbd
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-11-14 19:43:46 +0000

    ajustes no app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 function criaCartao(categoria, pergunta, resposta){
     //console.log(categoria, pergunta, resposta);
-}
 
-let cartao= document.createElement ("article");
-cartao.className = "cartao";
-cartao.innerHTML = `
+let cartao=document.createElement ("article");
+cartao.className= "cartao";
+cartao.innerHTML= `
             <div class="cartao__conteudo">
                 <h3>${categoria}</h3>
                 <div class="cartao__conteudo__pergunta">
@@ -15,5 +14,6 @@ cartao.innerHTML = `
                 </div>
             </div>
 `
-let container = document.getElementById('container');
+let container= document.getElementById('container');
 container.appendChild(cartao);
+}
